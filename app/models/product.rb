3bb562c5ab4c735ca65792a83c41daa43795218c
Nitin <nitin@nitin-laptop.(none)>
2013-10-23 01:34:51 +0530

    Devise checks

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ActiveRecord::Base
   attr_accessible :name, :price
+  validates_presence_of :name,:price
+  validates :price, :format => { :with => /^\d+??(?:\.\d{0,2})?$/ }, :numericality =>{:greater_than => 0}
 end
